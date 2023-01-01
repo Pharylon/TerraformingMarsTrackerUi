109779877af51f0a9a928a357e7867a6833676df
Zachary <me@zachshuford.com>
2023-01-01 16:00:13 -0500

    About to add Material UI animations

--- App.tsx
+++ App.tsx
@@ -1,11 +1,31 @@
 import { StatusBar } from 'expo-status-bar';
 import { ImageBackground, StyleSheet, Text, TextInput, View } from 'react-native';
 import { RecoilRoot, useRecoilState } from 'recoil';
-import { StatusBar as StatusBarNative} from 'react-native';
+import { Dimensions, Platform, StatusBar as StatusBarNative } from 'react-native';
 import React, { useState } from 'react';
-import UserName from './resources/UserName';
+import UserName from './components/UserName';
 import { userState } from './state/UserState';
 import Main from './components/Main';
+
+
+
+const X_WIDTH = 375;
+const X_HEIGHT = 812;
+
+const XSMAX_WIDTH = 414;
+const XSMAX_HEIGHT = 896;
+
+const { height, width } = Dimensions.get('window');
+
+const isIPhoneX = () => Platform.OS === 'ios' && !Platform.isPad && !Platform.isTVOS
+    ? width === X_WIDTH && height === X_HEIGHT || width === XSMAX_WIDTH && height === XSMAX_HEIGHT
+    : false;
+
+const StatusBarHeight = Platform.select({
+    ios: isIPhoneX() ? 44 : 20,
+    android: StatusBarNative.currentHeight,
+    default: 0
+})
 
 export default function App() {
   const image = require("./assets/mars.png");
@@ -16,13 +36,14 @@ export default function App() {
           <Main/>
         </ImageBackground>
     </View>
-    <StatusBar style="auto" />
+    <StatusBar backgroundColor='black' style="light" />
     </RecoilRoot>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
+    marginTop: StatusBarHeight,
     flex: 1
   },
   image: {
--- components/Main.tsx
+++ components/Main.tsx
@@ -1,15 +1,31 @@
 import { StyleSheet, Text, View } from 'react-native';
 import { useRecoilState } from 'recoil';
-import UserName from '../resources/UserName';
+import UserName from './UserName';
 import { userState } from '../state/UserState';
 
 const Main = () => {
   const [userNameState, setUserNameState] = useRecoilState(userState);
   return (
-    <View>
-      {!userNameState && <UserName/>}
+    <View style={styles.container}>
+      <View><Text style={styles.text} >{userNameState}FDKLJFLDKJ</Text></View>
+      <UserName/>
+      {/* {!userNameState && <UserName/>}
+      {!userNameState && <UserName/>} */}
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    display: "flex",
+    fontSize: 50,
+    color: "white"
+  },
+  text: {
+    color: "white",
+    fontSize: 50
+  }
+});
+
 
 export default Main;
