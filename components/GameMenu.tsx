adf7ab9edeafe371c27d4f5396db6e41a682ecce
Zachary <me@zachshuford.com>
2023-01-04 10:47:12 -0500

    Button style on game menu changed

--- components/GameMenu.tsx
+++ components/GameMenu.tsx
@@ -1,6 +1,6 @@
-import { Button, Input } from '@rneui/themed';
+import { Input } from '@rneui/themed';
 import { useState } from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button } from 'react-native';
 import { useRecoilState } from 'recoil';
 import { StartGame } from '../Connections/SignalR';
 import { gameStateAtom } from '../state/BoardState';
